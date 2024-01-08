6b518def62840ff47fa50c0005c7ab184521a9ec
Srinivasu Nakka <37465927+srinivasu-developer@users.noreply.github.com>
2024-01-08 22:05:44 +0530

    Update Factorial.java

--- Factorial.java
+++ Factorial.java
@@ -1,5 +1,6 @@
 public class Factorial {
 
+    /* Factorial of a number in linear time and constant auxilary space */
     public long factorial(int n) {
         long fact = 1L;
         for(int i = 2; i <= n; i++) {
