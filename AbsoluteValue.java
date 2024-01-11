e89b25d35c79e8e0f1c31129656a8fca34ce63b3
Srinivasu Nakka <37465927+srinivasu-developer@users.noreply.github.com>
2024-01-11 19:59:21 +0530

    Update AbsoluteValue.java

--- AbsoluteValue.java
+++ AbsoluteValue.java
@@ -1,4 +1,6 @@
-class AbsoluteValue {
+public class AbsoluteValue {
+
+    /* Finds absolute value of the given number */
     public int absolute(int i) {
         if(i < 0) {
             return i * -1;
