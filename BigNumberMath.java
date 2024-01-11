b5c1f49a9c511e30606367902303de030a9fc1a2
Srinivasu Nakka <37465927+srinivasu-developer@users.noreply.github.com>
2024-01-11 20:00:19 +0530

    Update BigNumberMath.java

--- BigNumberMath.java
+++ BigNumberMath.java
@@ -1,5 +1,6 @@
 public class BigNumberMath {
-  
+
+    /* Finds sum of 2 long numbers with modulus of 10^9 +7 */
     public static long sumUnderModulo(long a, long b){
         long mod = (long) Math.pow(10, 9) + 7;
         return (a % mod + b % mod) % mod;
