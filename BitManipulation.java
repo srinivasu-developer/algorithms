d5d419e477599e85acb8dcd9730831165a2585a5
Srinivasu Nakka <37465927+srinivasu-developer@users.noreply.github.com>
2024-01-12 22:28:58 +0530

    Update BitManipulation.java

--- BitManipulation.java
+++ BitManipulation.java
@@ -1,5 +1,9 @@
 public class BitManipulation
 {
-    
+    // Function to check if Kth bit is set or not.
+    static boolean checkKthBit(int n, int k)
+    {
+        return (n & (1 << k)) > 0;
+    }
     
 }
