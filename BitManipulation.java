b08b4082613ed01c5136e0b2d4563fd9241e7196
Srinivasu Nakka <37465927+srinivasu-developer@users.noreply.github.com>
2024-01-12 22:43:19 +0530

    Update BitManipulation.java

--- BitManipulation.java
+++ BitManipulation.java
@@ -5,5 +5,18 @@ public class BitManipulation
     {
         return (n & (1 << k)) > 0;
     }
+
+    //Function to find position of first set bit in the given number.
+    public static int getFirstSetBit(int n){
+        int position = 1;
+        while(n > 0) {
+            if((n & 1) > 0) {
+               return position;
+            }
+            ++position;
+            n >>= 1;
+        }
+        return 0;
+    }
     
 }
