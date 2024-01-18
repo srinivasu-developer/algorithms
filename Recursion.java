bed94daef8698b3394ee77f9a92c3fa16d4cc1e2
Srinivasu Nakka <37465927+srinivasu-developer@users.noreply.github.com>
2024-01-18 22:25:07 +0530

    Update Recursion.java
    sum of digits

--- Recursion.java
+++ Recursion.java
@@ -1,3 +1,11 @@
 /* Holds some of the recursion relation algorithms */
 public class Recursion {
+
+    public static int sumOfDigits(int n)
+    {
+        if(n <= 0) {
+            return 0;
+        }
+        return (n % 10) + sumOfDigits(n / 10);
+    }
 }
