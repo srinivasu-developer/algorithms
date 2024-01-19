6278742d46c7edca311db2c44e381a34fb88e5b5
Srinivasu Nakka <37465927+srinivasu-developer@users.noreply.github.com>
2024-01-19 21:53:06 +0530

    Update Recursion.java
    Fibanocci

--- Recursion.java
+++ Recursion.java
@@ -9,4 +9,13 @@ public class Recursion {
         }
         return (n % 10) + sumOfDigits(n / 10);
     }
+
+    /* Fibonacci series */
+    public static int fibonacci(int n)
+    {
+        if(n == 1 || n == 2) {
+            return 1;
+        }
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
 }
