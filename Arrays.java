a88d9c894ee37c752be258ecab64760a02365e35
Srinivasu Nakka <37465927+srinivasu-developer@users.noreply.github.com>
2024-01-21 15:13:45 +0530

    Update Arrays.java

--- Arrays.java
+++ Arrays.java
@@ -1,3 +1,26 @@
+/* Holds array related problems and solutions */
 public class Arrays {
-  
+
+  /* Largest and second largest number of the array */
+  public static ArrayList<Integer> largestAndSecondLargest(int arr[])
+    {
+        int sizeOfArray = arr.length;
+        ArrayList<Integer> largestAndSecondLargest = new ArrayList<>(2);
+        int largest = 0;
+        int second = -1;
+        for(int i = 0; i < sizeOfArray; i++) {
+            if (arr[i] > arr[largest]) {
+                second = largest;
+                largest = i;
+            } else if (arr[i] == arr[largest]) {
+                continue;
+            } else if (second == -1 || arr[i] > arr[second]) {
+                second = i;
+            }
+        }
+        int secondLargest = second == -1 ? -1 : arr[second];
+        largestAndSecondLargest.add(arr[largest]);
+        largestAndSecondLargest.add(secondLargest);
+        return largestAndSecondLargest;
+    }
 }
