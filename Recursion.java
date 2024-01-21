2ff12fabcb110b90a7e71260f6a564a4d0787675
Srinivasu Nakka <37465927+srinivasu-developer@users.noreply.github.com>
2024-01-21 15:11:25 +0530

    Update Recursion.java

--- Recursion.java
+++ Recursion.java
@@ -46,4 +46,14 @@ public class Recursion {
         }
         return ((n % 10) + digitalRoot(n / 10)) % 9;
     }
+
+    /* Josephus problem */
+    public int josephus(int n, int k)
+    {
+        if (n == 1) {
+            return 1;
+        } else {
+            return (josephus(n - 1, k) + k-1) % n + 1;
+        }
+    }
 }
