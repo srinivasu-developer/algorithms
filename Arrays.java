c90590d474519426d244eb5f10d46eee33aaeb6c
Srinivasu Nakka <37465927+srinivasu-developer@users.noreply.github.com>
2024-01-22 08:15:14 +0530

    Update Arrays.java

--- Arrays.java
+++ Arrays.java
@@ -2,22 +2,27 @@
 public class Arrays {
 
   /* Largest and second largest number of the array */
-  public static ArrayList<Integer> largestAndSecondLargest(int arr[])
-    {
-        int sizeOfArray = arr.length;
-        ArrayList<Integer> largestAndSecondLargest = new ArrayList<>(2);
-        int largest = 0;
-        int second = -1;
-        for(int i = 0; i < sizeOfArray; i++) {
-            if (arr[i] > arr[largest]) {
-                second = largest;
-                largest = i;
-            } else if (arr[i] != arr[largest] && (second == -1 || arr[i] > arr[second])) {
-                second = i;
-            }
-        }
-        largestAndSecondLargest.add(arr[largest]);
-        largestAndSecondLargest.add(second < 0 ? second : arr[second]);
-        return largestAndSecondLargest;
-    }
+  public static ArrayList<Integer> largestAndSecondLargest(int arr[]) {
+      int sizeOfArray = arr.length;
+      ArrayList<Integer> largestAndSecondLargest = new ArrayList<>(2);
+      int largest = 0;
+      int second = -1;
+      for(int i = 0; i < sizeOfArray; i++) {
+          if (arr[i] > arr[largest]) {
+              second = largest;
+              largest = i;
+          } else if (arr[i] != arr[largest] && (second == -1 || arr[i] > arr[second])) {
+              second = i;
+          }
+      }
+      largestAndSecondLargest.add(arr[largest]);
+      largestAndSecondLargest.add(second < 0 ? second : arr[second]);
+      return largestAndSecondLargest;
+  }
+
+  /* Insert an element at the end of the given array */
+  public void insertAtEnd(int arr[], int sizeOfArray, int element) {
+      arr[sizeOfArray - 1] = element;
+  }
+  
 }
