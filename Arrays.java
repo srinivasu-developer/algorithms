8987d565d5649b12619b7cb2e4a11ef90186fe23
Srinivasu Nakka <37465927+srinivasu-developer@users.noreply.github.com>
2024-01-24 19:54:51 +0530

    Update Arrays.java

--- Arrays.java
+++ Arrays.java
@@ -54,4 +54,16 @@ public class Arrays {
       }
       return leaders;
   }
+
+  /* Check if array is sorted in non-decreasing order */
+	public static boolean arraySortedOrNot(int arr[]) { 
+    int n = arr.length;
+		if (n != 0 && n != 1) {
+      for (int i = 1; i < n; i++) {
+  			if (arr[i - 1] > arr[i])
+  				return false;
+      }
+    }
+		return true;
+	}
 }
