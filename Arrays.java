6c57612c9d5562647717443cb200b8c5399c8d57
Srinivasu Nakka <37465927+srinivasu-developer@users.noreply.github.com>
2024-01-25 10:10:54 +0530

    Update Arrays.java

--- Arrays.java
+++ Arrays.java
@@ -78,4 +78,16 @@ public class Arrays {
 		end--;
 	}
   }	
+
+	public static int remDups(int arr[]) {
+		int n = arr.length;
+		int res = 1;
+		for(int i = 1; i < n; i++) {
+			if(arr[res - 1] != arr[i]) {
+				arr[res] = arr[i];
+				res++;
+			}
+		}
+		return res;
+	}
 }
