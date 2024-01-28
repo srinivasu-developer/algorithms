34b198d462e3b6cde824478025f7fe02ed3ea1ca
Srinivasu Nakka <37465927+srinivasu-developer@users.noreply.github.com>
2024-01-28 23:32:30 +0530

    Sorted array to wave array

--- Arrays.java
+++ Arrays.java
@@ -198,4 +198,14 @@ public class Arrays {
         }
         return maxLength;
     }
+
+	/* Converts the given sorted array to a wave array */
+	public static void convertToWave(int n, int[] a) {
+        int temp = 0;
+        for(int i = 0; i < n - 1; i = i + 2) {
+            temp = a[i];
+            a[i] = a[i + 1];
+            a[i + 1] = temp;
+        }
+    }
 }
