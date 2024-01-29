1cc420cd217828a10b3363a0ebf2c8ff41e225ba
Srinivasu Nakka <37465927+srinivasu-developer@users.noreply.github.com>
2024-01-29 11:48:27 +0530

    Update Arrays.java

--- Arrays.java
+++ Arrays.java
@@ -244,4 +244,26 @@ public class Arrays {
         }
         return max_sum;
     }
+
+	/* The group flips and number of less flips */
+	public static void printGroups(int arr[]) {
+		int n = arr.length;
+		// Traverse through all array elements starting from the second element
+		for(int i = 1; i < n; i++)
+		{	
+		// If current element is not same as previous
+			if (arr[i] != arr[i - 1])
+			{
+				// If it is same as first element then it is starting of the interval to be flipped.
+				if (arr[i] != arr[0])
+					System.out.print("From " + i + " to ");
+				// If it is not same as previous and same as first element, then previous element is end of interval
+				else
+					System.out.println(i - 1);
+			}
+		}
+		// Explicitly handling the end of last interval
+		if (arr[n - 1] != arr[0])
+			System.out.println(n - 1);
+	}
 }
