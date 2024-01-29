523713386978a874f91222ec7fbf98bf52d96f97
Srinivasu Nakka <37465927+srinivasu-developer@users.noreply.github.com>
2024-01-29 11:45:40 +0530

    Update Arrays.java

--- Arrays.java
+++ Arrays.java
@@ -229,4 +229,19 @@ public class Arrays {
         }
         return result;
     } 
+
+	/* Max sum with a sliding k elements subarray */
+	public static int maxSum(int arr[], int k)
+    {
+		int n = arr.length;
+        int curr_sum = 0;
+        for (int i = 0; i < k; i++)
+            curr_sum += arr[i];
+        int max_sum = curr_sum;
+        for (int i = k; i < n; i++) {
+            curr_sum += (arr[i] - arr[i - k]);
+            max_sum = Math.max(max_sum, curr_sum);
+        }
+        return max_sum;
+    }
 }
