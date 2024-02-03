1e845d691d73903bdfdac98db1c3c22ab4096e71
Srinivasu Nakka <37465927+srinivasu-developer@users.noreply.github.com>
2024-02-03 12:34:03 +0530

    Create IterativeSearch.java

---
+++ searching/IterativeSearch.java
@@ -0,0 +1,12 @@
+public class IterativeSearch{
+        
+    public static int search(int arr[], int x)
+    {
+        for(int i = 0; i < arr.length; i++) {
+            if(arr[i] == x) 
+                return i;
+        }
+        return -1;
+    }
+    
+}
