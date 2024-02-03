0fabc22536ade23e07d2b7991ca579f2cc81fb1d
Srinivasu Nakka <37465927+srinivasu-developer@users.noreply.github.com>
2024-02-03 12:43:02 +0530

    Update BinarySeach.java

--- searching/BinarySeach.java
+++ searching/BinarySeach.java
@@ -1,1 +1,22 @@
 
+public class BinarySeach {
+  
+    public static int searchInSorted(int arr[], int n, int k)
+    {
+        return searchInSorted(arr, 0, n - 1, k);
+    }
+    
+    private static int searchInSorted(int arr[], int lower, int end, int k) {
+        if(lower > end) {
+            return -1;
+        }
+        int middle = (lower + end) / 2;
+        if(arr[middle] == k) {
+            return 1;
+        } else if(arr[middle] > k) {
+            return searchInSorted(arr, lower, middle - 1, k);
+        } else {
+            return searchInSorted(arr, middle + 1, end, k);
+        }
+    }
+}
