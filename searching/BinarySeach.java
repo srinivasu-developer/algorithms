d2324aa5852606b978252f332bef42a61d77ee10
Srinivasu Nakka <37465927+srinivasu-developer@users.noreply.github.com>
2024-02-04 09:27:09 +0530

    Update BinarySeach.java

--- searching/BinarySeach.java
+++ searching/BinarySeach.java
@@ -60,6 +60,25 @@ public class BinarySeach {
             }
         }
         return result;
-        
+    }
+
+    public static int leftIndex(int n, int arr[], int x)
+    {
+       
+       int low = 0, high = n - 1, res = -1;
+		while (low <= high) {
+			// Normal Binary Search Logic
+			int mid = (low + high) / 2;
+			if (arr[mid] > x)
+				high = mid - 1;
+			else if (arr[mid] < x)
+				low = mid + 1;
+			// If arr[mid] is same as x, we update res and move to the left half.
+			else {
+				res = mid;
+				high = mid - 1;
+			}
+		}
+		return res;
     }
 }
