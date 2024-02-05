84ebef880b3aeb6c2513e7a371456381ca430d52
Srinivasu Nakka <37465927+srinivasu-developer@users.noreply.github.com>
2024-02-05 10:00:27 +0530

    Update BinarySeach.java

--- searching/BinarySeach.java
+++ searching/BinarySeach.java
@@ -137,4 +137,21 @@ public class BinarySeach {
 		}
 		return 0;		
 	}
+
+	/* Find the peak element in the array */
+	public static int findPeak(int arr[], int n)
+	{
+		// First or last element is peak element
+		if (n == 1 || arr[0] >= arr[1])
+			return 0;
+		if (arr[n - 1] >= arr[n - 2])
+			return n - 1;
+		// Check for every other element
+		for (int i = 1; i < n - 1; i++) {
+			// Check if the neighbors are smaller
+			if (arr[i] >= arr[i - 1] && arr[i] >= arr[i + 1])
+				return i;
+		}
+		return 0;
+	}
 }
