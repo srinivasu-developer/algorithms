cea8e79eae11c987a1f941becea1f306db31a9d8
Srinivasu Nakka <37465927+srinivasu-developer@users.noreply.github.com>
2024-02-05 09:57:24 +0530

    Update BinarySeach.java

--- searching/BinarySeach.java
+++ searching/BinarySeach.java
@@ -116,4 +116,25 @@ public class BinarySeach {
 		else 
 			return righIndex(arr, x) - first + 1;
 	}
+
+	/* Count ones in a binary sorted array */
+	public static int countOnes(int arr[])
+	{
+		int n = arr.length;
+		int low = 0, high = n - 1;
+		while(low <= high)
+		{
+			int mid = (low + high) / 2;
+			if(arr[mid] == 0)
+				low = mid + 1;
+			else
+			{
+				if(mid == 0 || arr[mid - 1] == 0)
+					return (n - mid);
+				else 
+					high = mid -1;
+			}
+		}
+		return 0;		
+	}
 }
