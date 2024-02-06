db88730fe06faca0d62f3a213a018c2afd102085
Srinivasu Nakka <37465927+srinivasu-developer@users.noreply.github.com>
2024-02-06 09:04:57 +0530

    Update BinarySeach.java

--- searching/BinarySeach.java
+++ searching/BinarySeach.java
@@ -171,4 +171,23 @@ public class BinarySeach {
 		}
 		return searchInSorted(arr, start, end, target);
 	}
+
+	/* Finds the peak from the array */
+	public static int findPeak(int arr[])
+	{
+		int n = arr.length;
+		int low = 0, high = n - 1;
+		while(low <= high)
+		{
+			int mid = (low + high) / 2;
+			if((mid == 0 || arr[mid - 1] <= arr[mid]) &&
+				(mid == n - 1 || arr[mid + 1] <= arr[mid]))
+				return mid;
+			if(mid > 0 && arr[mid - 1] >= arr[mid])
+				high = mid -1;
+			else
+				low = mid + 1;
+		}
+		return -1;
+	}
 }
