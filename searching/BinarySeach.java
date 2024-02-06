9562b12ce549b5075aeb04c954f2697362b88f73
Srinivasu Nakka <37465927+srinivasu-developer@users.noreply.github.com>
2024-02-06 09:12:47 +0530

    Update BinarySeach.java

--- searching/BinarySeach.java
+++ searching/BinarySeach.java
@@ -190,4 +190,25 @@ public class BinarySeach {
 		}
 		return -1;
 	}
+
+	/* Sum of elements in a array between 2 indices */
+	public static int sum(int arr[], int b, int e){
+        int s = 0;
+        for(int i = b; i <= e; i++)
+            s += arr[i];
+        return s;
+    }
+    
+    public static int minPages(int arr[], int k){
+		int n = arr.length;
+        if(k == 1)
+            return sum(arr, 0, n-1);
+        if(n == 1)
+            return arr[0];
+        int res = Integer.MAX_VALUE;
+        for(int i = 1; i < n; i++) {
+            res = Math.min(res, Math.max(minPages(arr, i, k-1), sum(arr, i, n-1)));
+        }
+        return res;
+    } 
 }
