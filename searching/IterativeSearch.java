0795d56ffbdc37f0242e3d0d2cc5e6f227199c10
Srinivasu Nakka <37465927+srinivasu-developer@users.noreply.github.com>
2024-02-07 06:48:52 +0530

    Update IterativeSearch.java

--- searching/IterativeSearch.java
+++ searching/IterativeSearch.java
@@ -40,5 +40,25 @@ public class IterativeSearch{
 		}
 		return i - 1;
 	}
+
+	/* Finds first and last occurance of the given number in array */
+	public static void findFirstAndLast(int arr[], int x)
+	{
+		int n = arr.length;
+		int first = -1, last = -1;
+		for (int i = 0; i < n; i++) {
+			if (x != arr[i])
+				continue;
+			if (first == -1)
+				first = i;
+			last = i;
+		}
+		if (first != -1) {
+			System.out.println("First Occurrence = "+ first);
+			System.out.println("Last Occurrence = " + last);
+		}
+		else
+			System.out.println("Not Found");
+	}
     
 }
