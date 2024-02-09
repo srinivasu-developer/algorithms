be231eb62a3057b687e35ccedaeb473f98ffca1d
Srinivasu Nakka <37465927+srinivasu-developer@users.noreply.github.com>
2024-02-09 12:42:54 +0530

    Update Math.java

--- mathematics/Math.java
+++ mathematics/Math.java
@@ -1,10 +1,19 @@
 public class Math {
   
-  public static int countDigit(long n)
-	{
+  	public static int countDigit(long n) {
 		if (n/10 == 0)
 			return 1;
 		return 1 + countDigit(n / 10);
+	}
+
+	public static boolean checkPalindrome(int n) {
+		int reverse = 0;
+		int temp = n;
+		while (temp != 0) {
+			reverse = (reverse * 10) + (temp % 10);
+			temp = temp / 10;
+		}
+		return (reverse == n); // if it is true then it will return 1 else if false it will return 0;
 	}
   
 }
