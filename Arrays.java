586a43a6cdc447d61bf5a66b7e7bd8d1c5d79357
Srinivasu Nakka <37465927+srinivasu-developer@users.noreply.github.com>
2024-02-12 08:27:01 +0530

    Update Arrays.java

--- Arrays.java
+++ Arrays.java
@@ -234,6 +234,29 @@ public class Arrays {
         }
     }
 
+	public static int maxWater(int[] arr)
+	{
+		int n = arr.length;
+		// To store the maximum water that can be stored
+		int res = 0;
+		// For every element of the array except first and last element
+		for (int i = 1; i < n - 1; i++) {
+			// Find maximum element on its left
+			int left = arr[i];
+			for (int j = 0; j < i; j++) {
+				left = Math.max(left, arr[j]);
+			}
+			// Find maximum element on its right
+			int right = arr[i];
+			for (int j = i + 1; j < n; j++) {
+				right = Math.max(right, arr[j]);
+			}
+			// Update maximum water value
+			res += Math.min(left, right) - arr[i];
+		}
+		return res;
+	}
+
 	/* Function to find the trapped water between the blocks. */
     public static long trappingWater(int arr[]) { 
 		int n = arr.length;
