653178b7a9fb45de55a14161248cc248b2d57deb
Srinivasu Nakka <37465927+srinivasu-developer@users.noreply.github.com>
2024-02-14 14:47:16 +0530

    Update BubbleSort.java

--- sorting/BubbleSort.java
+++ sorting/BubbleSort.java
@@ -1,3 +1,40 @@
 public class BubbleSort {
-  
+
+    public static void bubbleSort(int arr[])
+    {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // swap arr[j+1] and arr[j]
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void bubbleSortEffecient(int arr[])
+    {
+        int n = arr.length;
+        for(int i = 0; i < n - 1; i++) {
+            boolean swapped = false;
+            for(int j = 0; j < n - i - 1; j++) {
+                if(arr[j] > arr[j + 1]) {
+                    swap(arr, j, j + 1);
+                    swapped = true;
+                }
+            }
+            if(!swapped) {
+                return;
+            }
+        }
+    }
+    
+    private static void swap(int[] arr, int i, int j) {
+        arr[i] = arr[i] + arr[j];
+        arr[j] = arr[i] - arr[j];
+        arr[i] = arr[i] - arr[j];
+    }
 }
