ac149b4bd082cba8e4a02706185c9e119184b6e7
Srinivasu Nakka <37465927+srinivasu-developer@users.noreply.github.com>
2024-02-21 22:45:57 +0530

    Update MergeSort.java

--- sorting/MergeSort.java
+++ sorting/MergeSort.java
@@ -1,2 +1,18 @@
 public class MergeSort {
+
+    public static void mergeArrays(int[] arr1, int[] arr2, int[] arr3) {
+        int n1 = arr1.length;
+        int n2 = arr2.length;
+        int i = 0, j = 0, k = 0;
+        while (i < n1 && j < n2) {
+            if (arr1[i] < arr2[j])
+                arr3[k++] = arr1[i++];
+            else
+                arr3[k++] = arr2[j++];
+        }
+        while (i < n1)
+            arr3[k++] = arr1[i++];
+        while (j < n2)
+            arr3[k++] = arr2[j++];
+    }
 }
