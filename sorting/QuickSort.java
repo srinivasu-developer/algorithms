c913b9126f775c2418a7f76ea801e2377babbf25
Srinivasu Nakka <37465927+srinivasu-developer@users.noreply.github.com>
2024-02-23 20:03:52 +0530

    Update QuickSort.java

--- sorting/QuickSort.java
+++ sorting/QuickSort.java
@@ -1,2 +1,28 @@
 public class QuickSort {
+
+    public static int naivePartition(int a[], int start, int high)
+    {
+        int temp[] = new int[(high - start) + 1];
+        int pivot = a[high];
+        int index = 0;
+        // smaller number
+        for (int i = start; i <= high; ++i) {
+            if (a[i] < pivot) {
+                temp[index++] = a[i];
+            }
+        }
+        int position = index;
+        // Placing the pivot to its original position
+        temp[index++] = pivot;
+        for (int i = start; i <= high; ++i) {
+            if (a[i] > pivot) {
+                temp[index++] = a[i];
+            }
+        }
+        // Change the original array
+        for (int i = start; i <= high; ++i) {
+            a[i] = temp[i - start];
+        }
+        return position;
+    }
 }
