d22ffc68314762e29a5212ac27707c8eda15efe4
Srinivasu Nakka <37465927+srinivasu-developer@users.noreply.github.com>
2024-02-25 16:51:42 +0530

    Update Problems.java

--- sorting/Problems.java
+++ sorting/Problems.java
@@ -1,4 +1,15 @@
 public class Problems {
 
+    private static int lomutoPartition(int arr[], int low, int high) {
+        int pivot = arr[high];
+        int index = low - 1;
+        for(int i = low; i < high; i++) {
+            if(arr[i] < pivot) {
+            swap(arr, ++index, i);
+            }
+        }
+        swap(arr, high, ++index);
+        return index;
+    }
   
 }
