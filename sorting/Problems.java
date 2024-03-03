78fcb5219f19fbb1cc2bae5c2902a99f99cafbaf
Srinivasu Nakka <37465927+srinivasu-developer@users.noreply.github.com>
2024-03-03 09:29:04 +0530

    Update Problems.java

--- sorting/Problems.java
+++ sorting/Problems.java
@@ -40,6 +40,42 @@ public class Problems {
         return res;
     }
 
+	/* Cycle sort considering the duplicates elements in the array */
+	public static void cycleSort(int arr[]) {
+		int n = arr.length;
+		int writes = 0;
+		for (int cycleStart = 0; cycleStart <= n - 2; cycleStart++) {
+			int item = arr[cycleStart];
+			int pos = cycleStart;
+			for (int i = cycleStart + 1; i < n; i++)
+				if (arr[i] < item)
+					pos++;
+			if (pos == cycleStart)
+				continue;
+			while (item == arr[pos])
+				pos += 1;
+			if (pos != cycleStart) {
+				swap(arr, cycleStart, pos);
+				writes++;
+			}
+			// Rotate rest of the cycle
+			while (pos != cycleStart) {
+				pos = cycleStart;
+				for (int i = cycleStart + 1; i < n; i++)
+					if (arr[i] < item)
+						pos += 1;
+				while (item == arr[pos])
+					pos += 1;
+				if (item != arr[pos]) {
+					int temp = item;
+					item = arr[pos];
+					arr[pos] = temp;
+					writes++;
+				}
+			}
+		}
+	}
+
     /* Sort the given binary array */
     public static void binarySort(int arr[]) {
         int n = arr.length;
