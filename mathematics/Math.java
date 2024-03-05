b4c078d0edb602f237b88e85d354d080681b47de
Srinivasu Nakka <37465927+srinivasu-developer@users.noreply.github.com>
2024-03-05 13:16:59 +0530

    Update Math.java

--- mathematics/Math.java
+++ mathematics/Math.java
@@ -80,22 +80,24 @@ public class Math {
 	}
 
 	/* Finds the prime factors of the given number */
-	public static void primeFactors(int n) {
-		// Print the number of 2s that divide n
+	public static List<Integer> primeFactors(int n) {
+		if(n <= 1)
+			return Collections.emptyList();
+		List<Integer> primeFactors = new ArrayList<>();
 		while (n % 2 == 0) {
-			System.out.print(2 + " ");
+			primeFactors.add(2);
 			n /= 2;
 		}
 		// n must be odd at this point. So we can skip one element (Note i = i +2)
 		for (int i = 3; i <= Math.sqrt(n); i += 2) {
 			while (n % i == 0) {
-				System.out.print(i + " ");
+				primeFactors.add(i);
 				n /= i;
 			}
 		}
-		// This condition is to handle the case when n is a prime number greater than 2
 		if (n > 2)
-			System.out.print(n);
+			primeFactors.add(n);
+		return primeFactors;
 	}
 
 	/* Returns list of prime factors for the given number */
