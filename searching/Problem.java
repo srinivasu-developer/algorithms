ce7c03ab872da3cc8109e2c25b04bbc8968771be
Srinivasu Nakka <37465927+srinivasu-developer@users.noreply.github.com>
2024-03-10 12:33:41 +0530

    Update Problem.java

--- searching/Problem.java
+++ searching/Problem.java
@@ -1,2 +1,21 @@
 public class Problem {
+
+    public int countOccurence(int[] arr, int n, int k) {
+        int desired = n / k;
+        Map<Integer, Integer> countMap = new HashMap<>();
+        int currentElement;
+        int count = 0;
+        int currentCount;
+        for(int i = 0; i < n; i++) {
+            currentElement = arr[i];
+            countMap.putIfAbsent(currentElement, 0);
+            currentCount = countMap.get(currentElement);
+            countMap.put(currentElement, ++currentCount);
+            if(currentCount == desired + 1) {
+                ++count;
+            }
+        }
+        return count;
+    }
+    
 }
