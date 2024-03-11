dcb0f78f16b3c4a451aa0f9829b9c35318bec8c4
Srinivasu Nakka <37465927+srinivasu-developer@users.noreply.github.com>
2024-03-11 07:47:43 +0530

    Update Problem.java

--- searching/Problem.java
+++ searching/Problem.java
@@ -47,5 +47,22 @@ public class Problem {
         }
         return result;
     }
+
+    public static int leftIndex(int n, int arr[], int x) {  
+       int low = 0, high = n - 1, res = -1;
+		while (low <= high) {
+			int mid = (low + high) / 2;
+			if (arr[mid] > x)
+				high = mid - 1;
+			else if (arr[mid] < x)
+				low = mid + 1;
+			// If arr[mid] is same as x, we update res and move to the left half.
+			else {
+				res = mid;
+				high = mid - 1;
+			}
+		}
+		return res;
+    }
     
 }
