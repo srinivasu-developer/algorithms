4c2fb25501d12521e784db2f2a00a305846fdd03
Srinivasu Nakka <37465927+srinivasu-developer@users.noreply.github.com>
2024-03-15 15:31:02 +0530

    Update Problem.java

--- searching/Problem.java
+++ searching/Problem.java
@@ -111,5 +111,24 @@ public class Problem {
 		}
 		return -1;
 	}
+
+	public static int lastOcc(int arr[], int x) {
+		int n = arr.length;
+		int low = 0, high = n - 1;
+		while(low <= high) {
+			int mid = (low + high) / 2;
+			if(x > arr[mid])
+				low = mid + 1;
+			else if(x < arr[mid])
+				high = mid - 1;
+			else {
+				if(mid == n - 1 || arr[mid + 1] != arr[mid])
+					return mid;
+				else
+					low = mid + 1;
+			}
+		}
+		return -1;
+	}
     
 }
