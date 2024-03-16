e52668eacd90c55955c1c0114d997dcd3755b4e5
Srinivasu Nakka <37465927+srinivasu-developer@users.noreply.github.com>
2024-03-16 12:03:56 +0530

    Update Problem.java

--- searching/Problem.java
+++ searching/Problem.java
@@ -173,5 +173,20 @@ public class Problem {
 		}
 		return (int) ans;
 	}
+
+	/* Simple binary search algorithm */
+	public static int binarySearch(int arr[], int l, int r, int x) {
+		if (r >= l) {
+			int mid = l + (r - l)/2;
+			if (arr[mid] == x)
+				return mid;
+			if (arr[mid] > x)
+				return binarySearch(arr, l, mid-1, x);
+			return binarySearch(arr, mid+1, r, x);
+		}
+		return -1;
+	}
+
+	
     
 }
