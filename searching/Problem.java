7216aa424d055cc336296d0afc651c5320131871
Srinivasu Nakka <37465927+srinivasu-developer@users.noreply.github.com>
2024-03-16 12:00:17 +0530

    Update Problem.java

--- searching/Problem.java
+++ searching/Problem.java
@@ -141,5 +141,17 @@ public class Problem {
 		else 
 			return lastOcc(arr, x) - first + 1;
 	}
+
+	/* Returns floor of square root of x */
+	public static int floorSqrt(int x) {
+		if (x == 0 || x == 1)
+			return x;
+		// Starting from 1, try all numbers until i*i is greater than or equal to x.
+		int i = 1, result = 1;
+		while (result <= x) {
+			result = ++i * i;
+		}
+		return i - 1;
+	}
     
 }
