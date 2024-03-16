c34955a2c624949775733cf56f6b570bf85e7308
Srinivasu Nakka <37465927+srinivasu-developer@users.noreply.github.com>
2024-03-16 12:02:26 +0530

    Update Problem.java

--- searching/Problem.java
+++ searching/Problem.java
@@ -153,5 +153,25 @@ public class Problem {
 		}
 		return i - 1;
 	}
+
+	/* Returns floor of square root of x with binary search */
+	public static int floorSqrtBinarySearch(int x) {
+		if (x == 0 || x == 1)
+			return x;
+		long start = 1, end = x / 2, ans = 0;
+		while (start <= end) {
+			long mid = (start + end) / 2;
+			if (mid * mid == x)
+				return (int) mid;
+			// Since we need floor, we update answer when mid*mid is smaller than x, and move closer to sqrt(x)
+			if (mid * mid < x) {
+				start = mid + 1;
+				ans = mid;
+			}
+			else // If mid*mid is greater than x
+				end = mid - 1;
+		}
+		return (int) ans;
+	}
     
 }
