d6e4b2277428dc065f9b5b56912acb6a847d8523
Srinivasu Nakka <37465927+srinivasu-developer@users.noreply.github.com>
2024-03-18 18:56:33 +0530

    Update Problem.java

--- searching/Problem.java
+++ searching/Problem.java
@@ -226,6 +226,21 @@ public class Problem {
 		return -1;
 	}
 
+	public static int findPeakOptimized(int arr[]) {
+		int n = arr.length;
+		int low = 0, high = n - 1;
+		while(low <= high) {
+			int mid = (low + high) / 2;
+			if((mid == 0 || arr[mid - 1] <= arr[mid]) && (mid == n - 1 || arr[mid + 1] <= arr[mid]))
+				return mid;
+			if(mid > 0 && arr[mid - 1] >= arr[mid])
+				high = mid -1;
+			else
+				low = mid + 1;
+		}
+		return -1;
+	}
+
 	/* Finds the median of the 2 given arrays */
 	public static double median(int[] A, int[] B) {
 		int n = A.length;
