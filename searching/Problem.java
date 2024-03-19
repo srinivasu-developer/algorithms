2ae47b772199aaa022922366f51a5af7b5774a85
Srinivasu Nakka <37465927+srinivasu-developer@users.noreply.github.com>
2024-03-19 20:06:41 +0530

    Update Problem.java

--- searching/Problem.java
+++ searching/Problem.java
@@ -310,5 +310,25 @@ public class Problem {
 		}
 		return studentsRequired <= m;
 	}
+
+	public static int findPages(int arr[], int m) {
+		int n = arr.length;
+		int sum = 0;
+		if (n < m)
+			return -1;
+		for (int i = 0; i < n; i++)
+			sum += arr[i];
+		int start = arr[n - 1], end = sum;
+		int result = Integer.MAX_VALUE;
+		while (start <= end) {
+			int mid = start + (end - start) / 2;
+			if (isPossible(arr, m, mid)) {
+				result = mid;
+				end = mid - 1;
+			} else
+				start = mid + 1;
+		}
+		return result;
+	}
     
 }
