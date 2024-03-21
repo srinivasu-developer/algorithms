569acaeb0db72015e8c4d832c66fbf044bb2edb2
Srinivasu Nakka <37465927+srinivasu-developer@users.noreply.github.com>
2024-03-21 20:21:45 +0530

    Update Problems.java

--- matrix/Problems.java
+++ matrix/Problems.java
@@ -1,2 +1,14 @@
 public class Problems {
+
+    static int[][] sumMatrix(int a[][], int b[][]) {
+        if(a.length != b.length || a[0].length != b[0].length) {
+            return new int[0][0];
+        }
+        for(int i = 0; i < a.length; i++) {
+           for(int j = 0; j < a[0].length; j++) {
+               a[i][j] = a[i][j] + b[i][j];
+           }
+        }
+        return a;
+    }
 }
